cfbd819a8f10461ab6e5bff323df1d882d95c8bd
Valeria Jarquin <vjarquin99@gmail.com>
2024-04-13 01:16:44 -0600

    cambios de comentarios

--- ejercicio2.sh
+++ ejercicio2.sh
@@ -4,19 +4,19 @@
 # Uso: ./ejercicio2.sh <nombre_proceso> <comando_para_ejecutarlo>
 
 # Se asigna los argumentos a variables
-process_name=$1
-command_to_execute=$2
+nombre_proceso=$1
+comando_ejecutar=$2
 
 # Para monitorear el proceso se realiza un bucle
 while true; do
     # Verifica si el proceso está corriendo
-    pgrep -x "$process_name" >/dev/null
+    pgrep -x "$nombre_proceso" >/dev/null
 
     # Si el proceso no está corriendo, ejecuta el comando para levantarlo
     if [ $? -ne 0 ]; then
-        echo "No esta corriendo el proceso "$process_name". Volviendo a levantarlo..."
+        echo "No esta corriendo el proceso "$nombre_proceso". Volviendo a levantarlo..."
         # Se utliza nohup para que cuando el termine de ejecutar el script no deje de correr el proceso
-        nohup $command_to_execute >/dev/null 2>&1 &
+        nohup $comando_ejecutar >/dev/null 2>&1 &
     fi
 
     # Espera 5s antes de revisar nuevamente
