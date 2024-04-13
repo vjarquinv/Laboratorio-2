9a0fd27e938d1663ce981044eed3c408f61bc90a
Valeria Jarquin <vjarquin99@gmail.com>
2024-04-12 23:40:26 -0600

    Solucion ejercicio3 y archivos creados en las pruebas del codigo

--- ejercicio3.sh
+++ ejercicio3.sh
@@ -5,28 +5,44 @@
 # Argumento para guardar el ejecutable
 ejecutable="$1"
 
+# Se guarda en una variable la direccion del ejecutable
+dir_path=$(which $ejecutable)
+
 # Nombre del archivo de registro
 log_file="uso_cpu_mem.log"
 
 # Iniciar ejecución del binario en segundo plano
-./$ejecutable &
+$dir_path &
 
 # Para obtener el ID del proceso del ejecutable
 pid=$!
 
 # Función para obtener el uso de CPU y memoria y escribirlo en el archivo de registro
-monitor_process() {
+monitor_proceso() {
     while ps -p $pid > /dev/null; do
-        timestamp=$(date +%s)
-        cpu_usage=$(ps -p $pid -o %cpu | grep -v CPU)
-        mem_usage=$(ps -p $pid -o %mem | grep -v MEM)
-        echo "$timestamp $cpu_usage $mem_usage" >> $log_file
+        tiempo=$(date +%s)
+        uso_cpu=$(ps -p $pid -o %cpu | grep -v CPU)
+        uso_mem=$(ps -p $pid -o %mem | grep -v MEM)
+        echo "$tiempo $uso_cpu $uso_mem" >> $log_file
         sleep 1
     done
 }
 
 # Para monitorear el proceso en segundo plano
-monitor_process &
+monitor_proceso &
 
 # Esperar a que el proceso termine
 wait $pid
+
+gnuplot <<- EOF
+    set xlabel "Tiempo"
+    set ylabel "Uso (%)"
+    set title "Uso de CPU y Memoria en función del Tiempo"
+    set grid
+    set term png
+    set output "uso_cpu_mem.png"
+    plot "$log_file" using 1:2 with lines title "CPU", \
+         "$log_file" using 1:3 with lines title "Memoria"
+EOF
+
+echo "Grafico generado: uso_cpu_mem.png"
